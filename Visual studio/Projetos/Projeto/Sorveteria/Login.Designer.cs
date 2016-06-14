cff0fa2f24c27cbbec7b91b641b0cbecfc66c171
rafaelchewinski <rafael.chewinski@catolicasc.org.br>
2016-06-14 19:45:43 -0300

    Rafael
    Website

---
+++ Visual studio/Projetos/Projeto/Sorveteria/Login.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Sorveteria
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loginLabel = new System.Windows.Forms.Label();
+            this.senhaLabel = new System.Windows.Forms.Label();
+            this.loginTextBox = new System.Windows.Forms.TextBox();
+            this.senhaTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.nomeSorveteria = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // loginLabel
+            // 
+            this.loginLabel.AutoSize = true;
+            this.loginLabel.Location = new System.Drawing.Point(31, 63);
+            this.loginLabel.Name = "loginLabel";
+            this.loginLabel.Size = new System.Drawing.Size(33, 13);
+            this.loginLabel.TabIndex = 0;
+            this.loginLabel.Text = "Login";
+            // 
+            // senhaLabel
+            // 
+            this.senhaLabel.AutoSize = true;
+            this.senhaLabel.Location = new System.Drawing.Point(31, 124);
+            this.senhaLabel.Name = "senhaLabel";
+            this.senhaLabel.Size = new System.Drawing.Size(38, 13);
+            this.senhaLabel.TabIndex = 1;
+            this.senhaLabel.Text = "Senha";
+            // 
+            // loginTextBox
+            // 
+            this.loginTextBox.Location = new System.Drawing.Point(93, 63);
+            this.loginTextBox.Name = "loginTextBox";
+            this.loginTextBox.Size = new System.Drawing.Size(100, 20);
+            this.loginTextBox.TabIndex = 2;
+            this.loginTextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // senhaTextBox
+            // 
+            this.senhaTextBox.Location = new System.Drawing.Point(93, 124);
+            this.senhaTextBox.Name = "senhaTextBox";
+            this.senhaTextBox.Size = new System.Drawing.Size(100, 20);
+            this.senhaTextBox.TabIndex = 3;
+            this.senhaTextBox.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(74, 185);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Entrar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // nomeSorveteria
+            // 
+            this.nomeSorveteria.AutoSize = true;
+            this.nomeSorveteria.Location = new System.Drawing.Point(12, 9);
+            this.nomeSorveteria.Name = "nomeSorveteria";
+            this.nomeSorveteria.Size = new System.Drawing.Size(107, 13);
+            this.nomeSorveteria.TabIndex = 5;
+            this.nomeSorveteria.Text = "XUPAKI SORVETES";
+            this.nomeSorveteria.Click += new System.EventHandler(this.nomeSorveteria_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(233, 238);
+            this.Controls.Add(this.nomeSorveteria);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.senhaTextBox);
+            this.Controls.Add(this.loginTextBox);
+            this.Controls.Add(this.senhaLabel);
+            this.Controls.Add(this.loginLabel);
+            this.Name = "Login";
+            this.Text = "XUPAKI";
+            this.Load += new System.EventHandler(this.Login_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label loginLabel;
+        private System.Windows.Forms.Label senhaLabel;
+        private System.Windows.Forms.TextBox loginTextBox;
+        private System.Windows.Forms.TextBox senhaTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label nomeSorveteria;
+    }
+}
