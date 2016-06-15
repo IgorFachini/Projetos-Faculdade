a8e8a2436ecca6b7c94bb9409b069e6b74698e86
guiFagundes <guilhermefagundes96@gmail.com>
2016-06-14 21:01:13 -0300

    up

---
+++ visual/Projeto/Sorveteria/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sorveteria
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+<<<<<<< HEAD
+            Application.Run(new MontagemSorvete());
+=======
+            Application.Run(new Login());
+>>>>>>> origin/master
+        }
+    }
+}
---
+++ visual/Projeto/Sorveteria/TesteCobertura.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sorveteria {
+    class TesteCobertura : ISorvete {
+        //Component : IComponent
+        ISorvete s;
+
+
+        public TesteCobertura(ISorvete s) {
+            this.s = s;
+            MessageBox.Show("Add Morango!+2");
+        }
+
+        public string Nome {
+            get {
+                return this.s.Nome + " ADD Morango!";
+            }
+        }
+
+
+        //State
+        public double Preco {
+            get {
+                return this.s.Preco + 2;
+            }
+        }
+    }
+}
