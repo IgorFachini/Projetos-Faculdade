1c45759b91b6d15ea127a8854b6870ba4552f1e7
Igor Fachini <igor.fachini@unerj.br>
2016-06-18 20:45:56 -0300

    Up classes base

--- Visual studio/Projetos/Sorveteria/SorveteriaGamb/Program.cs
+++ Visual studio/Projetos/Sorveteria/SorveteriaGamb/Program.cs
@@ -16,7 +16,7 @@ namespace SorveteriaGamb
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Inicial());
         }
     }
 }
